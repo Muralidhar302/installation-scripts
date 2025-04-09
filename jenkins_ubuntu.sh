5484b5386c004633afface23131dc73b8c9d82da
Muralidhar302 <muralidhar302@gmail.com>
2025-04-09 16:09:56 +0530

    jenkins_ubuntu.sh

--- jenkins_ubuntu.sh
+++ jenkins_ubuntu.sh
@@ -8,13 +8,10 @@ sudo apt install -y openjdk-21-jdk
 echo "3. Adding Jenkins package to  apt list.............."
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
   https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
-  
-echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+ echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
   
-echo "4. updating apt.............."  
+echo "4. updating apt and Installing Jenkins.............."  
 sudo apt-get update
-
-echo "5. Installing Jenkins.............."
 sudo apt-get install jenkins
